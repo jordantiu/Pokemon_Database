9cf5b515377e9343062d5265a8330fff7416da43
matth1073 <matth1073@gmail.com>
2020-05-02 23:30:01 -0500

    basic query

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -10,15 +10,15 @@ app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_DB'] = 'Pokemon_Database'
 
-
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * from pokemon")
-    fetchdata = cur.fetchall()
-    cur.close()
-    return render_template('index.html', data = fetchdata)
-    
+    if request.method == "POST":
+        tID = request.form['trainerID']
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM COLLECTION WHERE trainer_id = %s" % (tID))
+        fetchdata = cur.fetchall()
+        return render_template('index.html', data = fetchdata)
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)

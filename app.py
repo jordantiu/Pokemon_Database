5e14c824933da1e79c3301ff51e95256702d4ec1
matth1073 <matth1073@gmail.com>
2020-05-02 21:25:57 -0500

    first connection

--- app.py
+++ app.py
@@ -3,10 +3,22 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+mysql = MySQL(app)
+
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_DB'] = 'Pokemon_Database'
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * from pokemon")
+    fetchdata = cur.fetchall()
+    cur.close()
+    return render_template('index.html', data = fetchdata)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
